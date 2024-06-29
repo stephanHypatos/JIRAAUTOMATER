d3098c599166a7e06a9dcf715f4d8aa3b43d9137
Stephan Kuche <stephan.kuche@hypatos.ai>
2024-06-29 14:39:07 +0200

    updated hello.py

--- HELLO.py
+++ HELLO.py
@@ -25,6 +25,7 @@ with st.expander('Key functionalities of the current version'):
         - It enables the establishment of projects within Jira, incorporating a structured hierarchy that spans accounts, projects, epics, tasks, and sub-tasks.
         - The app supports the delineation of issue dependencies, allowing for the specification of relationships such as "blocks" and "is blocked by".
         - It offers the capability to calculate the start and end dates of tasks, based on the specified duration for each issue type.
+        - You can provide the name of the project if you dont provide one the name of the newly created project will be the selected project type
 
         ### Weekly/Monthly Report Generation:
 
@@ -32,12 +33,16 @@ with st.expander('Key functionalities of the current version'):
         - It facilitates the creation of PowerPoint slide decks, aggregating and presenting the relevant project data succinctly.
         - Users are provided with the option to download the generated slide deck, enabling easy access and distribution of the report.
 
+        ### Delete Jira Issue:
+        - Delete a Jira Issue including all its children by providing the issue-key
+        - You can delete issues of type: project, epic or tasl
+        - You cant delete issues of type account
 
         ### Upcoming features
                     
         - Update the Timeline ( GANTT CHART THINK CELL ) if changes are made to the dates
         - Sharepoint Connection in order to make downloading and uploading of slidedecks unnecessary
-        - Update the projectplan in the app
+        - Update the underlying excel blueprint file in the app
         - Issue Updates: Updating existing Jira issues based on changes in an Excel file (including changes to summary, status, start date, due date, and description)
         - Transition Handling: Manages transitions between different statuses of Jira issues.
         """
